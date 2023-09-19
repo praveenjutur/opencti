9c8b15bc10ca215714b31d44b149073ecd910d38
Cathia Archidoit <cathia.archidoit@filigran.io>
2023-09-19 15:18:59 +0200

    [frontend/backend] StixCyberObservables filters & useCopy (#2686)

--- opencti-platform/opencti-graphql/src/generated/graphql.ts
+++ opencti-platform/opencti-graphql/src/generated/graphql.ts
@@ -14140,7 +14140,7 @@ export type MutationStixCyberObservablesExportAskArgs = {
   elementId?: InputMaybe<Scalars['String']['input']>;
   exportType: Scalars['String']['input'];
   filterMode?: InputMaybe<FilterMode>;
-  filters?: InputMaybe<Array<InputMaybe<StixCyberObservablesFiltering>>>;
+  filters?: InputMaybe<StixCyberObservablesGroupFiltering>;
   format: Scalars['String']['input'];
   maxMarkingDefinition?: InputMaybe<Scalars['String']['input']>;
   orderBy?: InputMaybe<StixCyberObservablesOrdering>;
@@ -19645,7 +19645,7 @@ export type QueryStixCyberObservableArgs = {
 export type QueryStixCyberObservablesArgs = {
   after?: InputMaybe<Scalars['ID']['input']>;
   filterMode?: InputMaybe<FilterMode>;
-  filters?: InputMaybe<Array<InputMaybe<StixCyberObservablesFiltering>>>;
+  filters?: InputMaybe<StixCyberObservablesGroupFiltering>;
   first?: InputMaybe<Scalars['Int']['input']>;
   orderBy?: InputMaybe<StixCyberObservablesOrdering>;
   orderMode?: InputMaybe<OrderingMode>;
@@ -19659,7 +19659,7 @@ export type QueryStixCyberObservablesDistributionArgs = {
   dateAttribute?: InputMaybe<Scalars['String']['input']>;
   field: Scalars['String']['input'];
   filterMode?: InputMaybe<FilterMode>;
-  filters?: InputMaybe<Array<InputMaybe<StixCyberObservablesFiltering>>>;
+  filters?: InputMaybe<StixCyberObservablesGroupFiltering>;
   objectId?: InputMaybe<Scalars['String']['input']>;
   operation: Scalars['String']['input'];
   search?: InputMaybe<Scalars['String']['input']>;
@@ -19676,7 +19676,7 @@ export type QueryStixCyberObservablesNumberArgs = {
   authorId?: InputMaybe<Scalars['String']['input']>;
   endDate?: InputMaybe<Scalars['DateTime']['input']>;
   filterMode?: InputMaybe<FilterMode>;
-  filters?: InputMaybe<Array<InputMaybe<StixCyberObservablesFiltering>>>;
+  filters?: InputMaybe<StixCyberObservablesGroupFiltering>;
   search?: InputMaybe<Scalars['String']['input']>;
   types?: InputMaybe<Array<InputMaybe<Scalars['String']['input']>>>;
 };
@@ -19684,7 +19684,7 @@ export type QueryStixCyberObservablesNumberArgs = {
 
 export type QueryStixCyberObservablesTimeSeriesArgs = {
   filterMode?: InputMaybe<FilterMode>;
-  filters?: InputMaybe<Array<InputMaybe<StixCyberObservablesFiltering>>>;
+  filters?: InputMaybe<StixCyberObservablesGroupFiltering>;
   search?: InputMaybe<Scalars['String']['input']>;
   types?: InputMaybe<Array<InputMaybe<Scalars['String']['input']>>>;
 };
@@ -22923,11 +22923,17 @@ export enum StixCyberObservablesFilter {
   XOpenctiScore = 'x_opencti_score'
 }
 
-export type StixCyberObservablesFiltering = {
-  filterMode?: InputMaybe<FilterMode>;
+export type StixCyberObservablesFilterFiltering = {
   key: Array<StixCyberObservablesFilter>;
+  mode?: InputMaybe<FilterMode>;
   operator?: InputMaybe<Scalars['String']['input']>;
-  values?: InputMaybe<Array<InputMaybe<Scalars['String']['input']>>>;
+  values: Array<InputMaybe<Scalars['String']['input']>>;
+};
+
+export type StixCyberObservablesGroupFiltering = {
+  filterGroups: Array<InputMaybe<StixCyberObservablesGroupFiltering>>;
+  filters: Array<InputMaybe<StixCyberObservablesFilterFiltering>>;
+  mode: FilterMode;
 };
 
 export enum StixCyberObservablesOrdering {
@@ -29386,7 +29392,8 @@ export type ResolversTypes = ResolversObject<{
   StixCyberObservableEdge: ResolverTypeWrapper<Omit<StixCyberObservableEdge, 'node'> & { node: ResolversTypes['StixCyberObservable'] }>;
   StixCyberObservableEditMutations: ResolverTypeWrapper<Omit<StixCyberObservableEditMutations, 'contextClean' | 'contextPatch' | 'fieldPatch' | 'promote' | 'relationAdd' | 'relationDelete' | 'relationsAdd'> & { contextClean?: Maybe<ResolversTypes['StixCyberObservable']>, contextPatch?: Maybe<ResolversTypes['StixCyberObservable']>, fieldPatch?: Maybe<ResolversTypes['StixCyberObservable']>, promote?: Maybe<ResolversTypes['StixCyberObservable']>, relationAdd?: Maybe<ResolversTypes['StixRefRelationship']>, relationDelete?: Maybe<ResolversTypes['StixCyberObservable']>, relationsAdd?: Maybe<ResolversTypes['StixCyberObservable']> }>;
   StixCyberObservablesFilter: StixCyberObservablesFilter;
-  StixCyberObservablesFiltering: StixCyberObservablesFiltering;
+  StixCyberObservablesFilterFiltering: StixCyberObservablesFilterFiltering;
+  StixCyberObservablesGroupFiltering: StixCyberObservablesGroupFiltering;
   StixCyberObservablesOrdering: StixCyberObservablesOrdering;
   StixDomainObject: ResolverTypeWrapper<ResolversInterfaceTypes<ResolversTypes>['StixDomainObject']>;
   StixDomainObjectAddInput: StixDomainObjectAddInput;
@@ -30113,7 +30120,8 @@ export type ResolversParentTypes = ResolversObject<{
   StixCyberObservableConnection: Omit<StixCyberObservableConnection, 'edges'> & { edges: Array<ResolversParentTypes['StixCyberObservableEdge']> };
   StixCyberObservableEdge: Omit<StixCyberObservableEdge, 'node'> & { node: ResolversParentTypes['StixCyberObservable'] };
   StixCyberObservableEditMutations: Omit<StixCyberObservableEditMutations, 'contextClean' | 'contextPatch' | 'fieldPatch' | 'promote' | 'relationAdd' | 'relationDelete' | 'relationsAdd'> & { contextClean?: Maybe<ResolversParentTypes['StixCyberObservable']>, contextPatch?: Maybe<ResolversParentTypes['StixCyberObservable']>, fieldPatch?: Maybe<ResolversParentTypes['StixCyberObservable']>, promote?: Maybe<ResolversParentTypes['StixCyberObservable']>, relationAdd?: Maybe<ResolversParentTypes['StixRefRelationship']>, relationDelete?: Maybe<ResolversParentTypes['StixCyberObservable']>, relationsAdd?: Maybe<ResolversParentTypes['StixCyberObservable']> };
-  StixCyberObservablesFiltering: StixCyberObservablesFiltering;
+  StixCyberObservablesFilterFiltering: StixCyberObservablesFilterFiltering;
+  StixCyberObservablesGroupFiltering: StixCyberObservablesGroupFiltering;
   StixDomainObject: ResolversInterfaceTypes<ResolversParentTypes>['StixDomainObject'];
   StixDomainObjectAddInput: StixDomainObjectAddInput;
   StixDomainObjectConnection: Omit<StixDomainObjectConnection, 'edges'> & { edges?: Maybe<Array<Maybe<ResolversParentTypes['StixDomainObjectEdge']>>> };
